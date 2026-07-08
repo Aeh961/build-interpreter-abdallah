a75cc4b7a155df817d17f99cc9b10a381adee30f
AEH <117328917+Abdallahelhamawi@users.noreply.github.com>
2026-07-08 14:20:49 -0700

    Look up variables in the environment

--- workshop4/workshop4_hw/src/environment.py
+++ workshop4/workshop4_hw/src/environment.py
@@ -6,8 +6,9 @@ class Environment:
         self.values[name] = value
 
     def get(self, name: str):
-        # TODO: return the value if it exists; otherwise raise NameError.
-        raise NotImplementedError("TODO: implement get()")
+        if name in self.values:
+            return self.values[name]
+        raise NameError(f"Variable {name!r} is not defined")
 
     def assign(self, name: str, value):
         # TODO: update an existing name; otherwise raise NameError.
